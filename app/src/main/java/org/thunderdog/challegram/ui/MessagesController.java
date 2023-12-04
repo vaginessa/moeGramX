85169665809ebd47e955cbd5b00896b9bbb068ee
Kira Roubin <kirao@duck.com>
2023-12-04 19:30:32 +0300

    fix: Reflect TdApi method changes

--- app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
+++ app/src/main/java/org/thunderdog/challegram/ui/MessagesController.java
@@ -5551,7 +5551,7 @@ public class MessagesController extends ViewController<MessagesController.Argume
         });
       } else if (id == R.id.btn_msgRepeat) {
         TdApi.Message msg = selectedMessage.getMessage();
-        tdlib.client().send(new TdApi.ForwardMessages(msg.chatId, msg.messageThreadId, msg.chatId, new long[] {msg.id}, null, true, false, false), tdlib.messageHandler());
+        tdlib.client().send(new TdApi.ForwardMessages(msg.chatId, msg.messageThreadId, msg.chatId, new long[] {msg.id}, null, true, false), tdlib.messageHandler());
       } else if (id == R.id.btn_openIn) {
         if (selectedMessageTag != null) {
           TdApi.Document document = ((TdApi.MessageDocument) selectedMessage.getMessage().content).document;

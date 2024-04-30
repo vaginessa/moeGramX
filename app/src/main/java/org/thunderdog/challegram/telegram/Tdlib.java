f0feb0bcb5dbd0b7e470655fc3986b6effa46a1a
Kira Roubin <kirao@duck.com>
2024-05-01 00:54:05 +0300

    chore: Replace updater file prefix

--- app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
+++ app/src/main/java/org/thunderdog/challegram/telegram/Tdlib.java
@@ -10820,7 +10820,7 @@ public class Tdlib implements TdlibProvider, Settings.SettingsChangeListener, Da
         int buildNo = 0;
         String version = null;
         String commit = null;
-        final String prefix = "Moegram-X-";
+        final String prefix = "moeGramX-";
         if (!StringUtils.isEmpty(document.fileName) && document.fileName.startsWith(prefix)) {
           int i = document.fileName.indexOf('-', prefix.length());
           version = document.fileName.substring(prefix.length(), i == -1 ? document.fileName.length() : i);
